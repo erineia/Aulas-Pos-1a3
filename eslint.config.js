8a2768bcaf50e8841820c26baa1364c45ce0c6ae
erineia.silva <erineia.silva@cnj.jus.br>
2025-06-27 12:07:06 -0300

    Ci: ajustes 2 pepi n3 e n4

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// eslint.config.js
+export default [
+  {
+    files: ['**/*.js'],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-unused-vars': 'warn'
+    }
+  }
+];
